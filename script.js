06a3ec185ae7d035af8867e564a6b4ed8246947d
Carsten Petschel <cp7781@gmail.com>
2020-11-15 09:33:49 +0100

    add script

---
+++ script.js
@@ -0,0 +1,17 @@
+function change() {
+    let timeout = 666;
+    let message = document.getElementById('message');
+    let text = message.textContent;
+    let length = text.length
+    let index = 0;
+    function changeMessage() {
+        ++index;
+        index %= length;
+        let text2 = text.substring(0, index);
+        text2 += text[index].toUpperCase();
+        text2 += text.substring(index + 1, length);
+        message.innerHTML = text2;
+        window.setTimeout(changeMessage, timeout);
+    }
+    window.setTimeout(changeMessage, timeout);
+}
