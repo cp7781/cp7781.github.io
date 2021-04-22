61c0e097f2c296424b93dc6e8237cb30009273d8
Carsten Petschel <cp7781@gmail.com>
2021-04-22 19:44:50 +0200

    Prepare a canvas to draw moving balls

--- script.js
+++ script.js
@@ -12,3 +12,39 @@ function change() {
         message.innerHTML = text.substring(0, index) + text[index].toUpperCase() + text.substring(index + 1, length);
     }, timeout);
 };
+
+function drawBalls() {
+
+    function random(minimum, maximum) {
+        return Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
+    }
+
+    class Ball {
+
+        constructor(x, y, velocityX, velocityY, color, radius) {
+            this.x = x;
+            this.y = y;
+            this.velocityX = velocityX;
+            this.velocityY = velocityY;
+            this.color = color;
+            this.radius = radius;
+        }
+
+        draw(drawboardContext) {
+            drawboardContext.beginPath();
+            drawboardContext.fillStyle = this.color;
+            drawboardContext.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+            drawboardContext.fill();
+            return drawboardContext;
+        }
+
+    }
+
+    let drawboard = document.getElementById('drawboard');
+
+    let drawboardContext = drawboard.getContext('2d');
+
+    let testBall = new Ball(50, 100, 4, 4, 'white', 10);
+    testBall.draw(drawboardContext);
+
+}
