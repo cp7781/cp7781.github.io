4055714f7b451aefa9d492a4361198db5b69eb5b
Carsten Petschel <cp7781@gmail.com>
2021-04-30 21:20:39 +0200

    added some color

--- script.js
+++ script.js
@@ -18,80 +18,174 @@ function changeMessage() {
 
 function drawCircles() {
 
+    class Color {
+
+        constructor(red, green, blue, alpha) {
+            this._red = red;
+            this._green = green;
+            this._blue = blue;
+            this._alpha = alpha;
+        }
+
+        get rgba() {
+            return `rgba(${this._red}, ${this._green}, ${this._blue}, ${this._alpha})`;
+        }
+
+        set red(red) {
+            if (red < 173) {
+                red = 173;
+            } else if (red > 255) {
+                red = 255;
+            }
+            this._red = red;
+        }
+
+        get red() {
+            return this._red;
+        }
+
+        set green(green) {
+            if (green < 173) {
+                green = 173;
+            } else if (green > 255) {
+                green = 255;
+            }
+            this._green = green;
+        }
+
+        get green() {
+            return this._green;
+        }
+
+        set blue(blue) {
+            if (blue < 173) {
+                blue = 173;
+            } else if (blue > 255) {
+                blue = 255;
+            }
+            this._blue = blue;
+        }
+
+        get blue() {
+            return this._blue;
+        }
+
+        set alpha(alpha) {
+            if (alpha < 0) {
+                alpha = 0;
+            } else if (alpha > 1) {
+                alpha = 1;
+            }
+            this._alpha = alpha;
+        }
+
+        get alpha() {
+            return this._alpha;
+        }
+
+    }
+
     class Circle {
-        constructor(radius, x, y, velocityX, velocityY) {
+
+        constructor(radius, x, y, velocityX, velocityY, color, drawboard) {
             this.radius = radius;
             this.x = x;
             this.y = y;
             this.velocityX = velocityX;
             this.velocityY = velocityY;
+            this.color = color;
+            this.drawboard = drawboard;
         }
-    }
 
-    function drawCircle(circle, drawboardContext) {
-        drawboardContext.beginPath();
-        drawboardContext.fillStyle = 'rgba(255, 255, 255, .68)';
-        drawboardContext.arc(circle.x, circle.y, circle.radius, 0, 2 * Math.PI);
-        drawboardContext.fill();
-    }
+        static generateRandomCircle(drawboard) {
 
-    function moveCircle(circle, drawboardWidth, drawboardHeight) {
-        if ((circle.x + circle.radius) >= drawboardWidth) {
-            circle.velocityX *= -1;
+            const smallestSideLength = drawboard.width > drawboard.height ? drawboard.height : drawboard.width;
+            const minimumRadius = Math.floor(smallestSideLength * .004);
+            const maximumRadius = Math.floor(smallestSideLength * .036);
+
+            const radius = generateRandomInteger(minimumRadius, maximumRadius);
+            const x = generateRandomInteger(radius, drawboard.width - radius);
+            const y = generateRandomInteger(radius, drawboard.height - radius);
+            const velocityX = generateRandomInteger(1, 3);
+            const velocityY = generateRandomInteger(1, 3);
+            const color = new Color(
+                generateRandomInteger(173, 255),
+                generateRandomInteger(173, 255),
+                generateRandomInteger(173, 255),
+                0.68
+            );
+
+            return new Circle(radius, x, y, velocityX, velocityY, color, drawboard);
+
         }
-        if ((circle.x - circle.radius) <= 0) {
-            circle.velocityX *= -1;
+
+        adjustColor() {
+            const value = generateRandomInteger(0, 1) > 0 ? 1 : -1;
+            switch (generateRandomInteger(0, 3)) {
+                case 0:
+                    this.color.red += value;
+                    break;
+                case 1:
+                    this.color.green += value;
+                    break;
+                case 2:
+                    this.color.blue += value;
+                    break;
+            }
         }
-        if ((circle.y + circle.radius) >= drawboardHeight) {
-            circle.velocityY *= -1;
+
+        move() {
+            if ((this.x + this.radius) >= this.drawboard.width) {
+                this.velocityX *= -1;
+            }
+            if ((this.x - this.radius) <= 0) {
+                this.velocityX *= -1;
+            }
+            if ((this.y + this.radius) >= this.drawboard.height) {
+                this.velocityY *= -1;
+            }
+            if ((this.y - this.radius) <= 0) {
+                this.velocityY *= -1;
+            }
+            this.x += this.velocityX;
+            this.y += this.velocityY;
         }
-        if ((circle.y - circle.radius) <= 0) {
-            circle.velocityY *= -1;
+
+        draw() {
+            const drawboardContext = drawboard.getContext('2d');
+            drawboardContext.beginPath();
+            drawboardContext.fillStyle = this.color.rgba;
+            drawboardContext.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+            drawboardContext.fill();
         }
-        circle.x += circle.velocityX;
-        circle.y += circle.velocityY;
+
     }
 
     function generateRandomInteger(minimum, maximum) {
         return Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
     }
 
-    function generateRandomCircle(drawboardWidth, drawboardHeight) {
-
-        const smallestSideLength = drawboardWidth > drawboardHeight ? drawboardHeight : drawboardWidth;
-        const minimumRadius = Math.floor(smallestSideLength * .004);
-        const maximumRadius = Math.floor(smallestSideLength * .036);
-
-        const radius = generateRandomInteger(minimumRadius, maximumRadius);
-        const x = generateRandomInteger(radius, drawboardWidth - radius);
-        const y = generateRandomInteger(radius, drawboardHeight - radius);
-        const velocityX = generateRandomInteger(1, 3);
-        const velocityY = generateRandomInteger(1, 3);
-
-        return new Circle(radius, x, y, velocityX, velocityY);
-
-    }
-
     {
 
         const drawboard = document.getElementById('drawboard');
-        const drawboardContext = drawboard.getContext('2d');
         drawboard.width = window.innerWidth;
         drawboard.height = window.innerHeight;
 
         const circles = new Array();
         for (let index = 0; index < 9; index++) {
-            circles.push(generateRandomCircle(drawboard.width, drawboard.height));
+            circles.push(Circle.generateRandomCircle(drawboard));
         }
 
         let animationStopped = false;
 
         function animateCircles() {
+            const drawboardContext = drawboard.getContext('2d');
             drawboardContext.fillStyle = 'rgb(47, 79, 79)';
             drawboardContext.fillRect(0, 0, drawboard.width, drawboard.height);
             circles.forEach(circle => {
-                moveCircle(circle, drawboard.width, drawboard.height);
-                drawCircle(circle, drawboardContext);
+                circle.adjustColor();
+                circle.move();
+                circle.draw();
             });
             if (!animationStopped) {
                 requestAnimationFrame(animateCircles);
