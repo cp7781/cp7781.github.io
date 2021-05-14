d80754019d8f91d58c7af2aa320cc42b60d7b6ad
Carsten Petschel <cp7781@gmail.com>
2021-05-14 19:42:53 +0200

    stucked on a library test

---
+++ modules/test.mjs
@@ -0,0 +1,30 @@
+import 'https://cdnjs.cloudflare.com/ajax/libs/matter-js/0.17.1/matter.js'
+
+export function test() {
+    let Engine = Matter.Engine,
+        Render = Matter.Render,
+        World = Matter.World,
+        Bodies = Matter.Bodies;
+
+    let engine = Engine.create();
+
+    let render = Render.create({
+        element: document.body,
+        engine: engine,
+        options: {
+            width: 800,
+            height: 400,
+            wireframes: false
+        }
+    });
+
+    let boxA = Bodies.rectangle(400, 200, 80, 80);
+    let ballA = Bodies.circle(380, 100, 40, 10);
+    let ballB = Bodies.circle(460, 10, 40, 10);
+    let ground = Bodies.rectangle(400, 380, 810, 60, { isStatic: true });
+
+    World.add(engine.world, [boxA, ballA, ballB, ground]);
+
+    Engine.run(engine);
+    Render.run(render);
+}
--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 import * as Message from './modules/changeMessage.mjs'
 import * as Circles from './modules/drawCircles.mjs'
+import * as Test from './modules/test.mjs'
 
 window.addEventListener('load', event => {
     Message.change()
     Circles.draw()
+    Test.test()
 })
