60a22c0401f9c7779fd1420d3f73281e72302469
Carsten Petschel <cp7781@gmail.com>
2021-05-15 09:56:18 +0200

    make use of Composite

--- modules/testPhysics.mjs
+++ modules/testPhysics.mjs
@@ -3,6 +3,8 @@ import Matter from 'https://cdn.skypack.dev/pin/matter-js@v0.17.1-x9Mw93jy1Lkhmw
 export default function () {
 
     const engine = Matter.Engine.create();
+    engine.world.gravity.x = -.05;
+    engine.world.gravity.y = .05;
 
     const render = Matter.Render.create({
         element: document.body,
@@ -19,7 +21,7 @@ export default function () {
     const ballB = Matter.Bodies.circle(460, 10, 10);
     const ground = Matter.Bodies.rectangle(300, 290, 400, 10, { isStatic: true });
 
-    Matter.World.add(engine.world, [boxA, ballA, ballB, ground]);
+    Matter.Composite.add(engine.world, [boxA, ballA, ballB, ground]);
 
     Matter.Runner.run(engine);
     Matter.Render.run(render);
