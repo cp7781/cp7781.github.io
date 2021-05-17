62da761c0e4319b6f1b2bf4dd08a4cb0ec3696ea
Carsten Petschel <cp7781@gmail.com>
2021-05-17 19:38:04 +0200

    changes for a crispier font at the bottom line

--- modules/drawBackground.mjs
+++ modules/drawBackground.mjs
@@ -369,19 +369,19 @@ export default function () {
             }
             let font = `'Source Sans Pro'`
             if (this.canvas.width > this.canvas.height) {
-                coordinate.x = Math.round(this.canvas.height * .01)
-                coordinate.y = Math.round(this.canvas.height - coordinate.x * 1.3)
-                font = `${Math.ceil(this.canvas.height * .015)}px ${font}`
+                coordinate.x = this.canvas.height * .01
+                coordinate.y = this.canvas.height - coordinate.x * 1.75
+                font = `${this.canvas.height * .03}px ${font}`
             } else {
-                coordinate.x = Math.round(this.canvas.width * .01)
-                coordinate.y = Math.round(this.canvas.height - coordinate.x * 1.3)
-                font = `${Math.ceil(this.canvas.width * .015)}px ${font}`
+                coordinate.x = this.canvas.width * .01
+                coordinate.y = this.canvas.height - coordinate.x * 1.75
+                font = `${this.canvas.width * .03}px ${font}`
             }
 
             const context = this.canvas.getContext('2d')
             context.fillStyle = 'seashell'
             context.font = font
-            context.fillText(`${averageFPS} fps`, coordinate.x, coordinate.y)
+            context.fillText(`⍈ ${averageFPS} fps`, coordinate.x, coordinate.y)
 
         }
 
