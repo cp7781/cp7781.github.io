a7305dff54e3f1b80ab8ef3e2e8a4a64dcab60fd
Carsten Petschel <cp7781@gmail.com>
2021-05-21 19:34:24 +0200

    added a quote request

---
+++ modules/requestQuote.mjs
@@ -0,0 +1,10 @@
+export default function () {
+    const request = new XMLHttpRequest()
+    request.responseType = 'json'
+    request.addEventListener('load', (event) => {
+        const quote = event.target.response.data[0];
+        document.querySelector('#quote').innerHTML = `${quote.quoteText} - ${quote.quoteAuthor}`
+    })
+    request.open("GET", "https://quote-garden.herokuapp.com/api/v3/quotes/random")
+    request.send()
+}
--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 import changeMessage from './modules/changeMessage.mjs'
 import drawBackground from './modules/drawBackground.mjs'
 import drawSymbol from './modules/drawSymbol.mjs'
+import requestQuote from './modules/requestQuote.mjs'
 
 window.addEventListener('load', event => {
     changeMessage()
     drawBackground()
     drawSymbol()
+    requestQuote()
 })
