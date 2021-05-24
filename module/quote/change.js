7b876cca947704b90f1a5d99b2bd6f7d554b0e1e
Carsten Petschel <cp7781@gmail.com>
2021-05-24 16:04:57 +0200

    changed the encoding method

--- module/quote/change.js
+++ module/quote/change.js
@@ -8,7 +8,7 @@ export default function () {
     request.addEventListener('load', event => {
         const data = event.target.response.data[0]
         const quote = document.querySelector('#quote')
-        quote.innerHTML = `${data.quoteText}<br># <a href="https://www.google.com/search?q=${encodeURI(data.quoteAuthor)}" target="_blank">${data.quoteAuthor}</a>`
+        quote.innerHTML = `${data.quoteText}<br># <a href="https://www.google.com/search?q=${encodeURIComponent(data.quoteAuthor)}" target="_blank">${data.quoteAuthor}</a>`
         quote.style.visibility = 'visible';
     })
     request.open("GET", "https://quote-garden.herokuapp.com/api/v3/quotes/random")
